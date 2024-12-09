e62cc42a44f6655c3bbfb17b38b7c78e606cea15
shyama-d3v <shyama@d3vtech.com>
2024-12-09 16:17:22 +0530

    add

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	wecome := "welcome to user input"
 	fmt.Println(wecome)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter the rating for our Pizza:")
+	//comma ok || comma ok
+	input, _ := reader.ReadString('\n')
+	fmt.Println("Thanks for rating,", input)
+	fmt.Printf("variable of type: %T \n", input)
 }
